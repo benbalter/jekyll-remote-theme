1879eb0276e9c66465a05144bb771813432c6a1f
Michael Gerzabek <michael.gerzabek@gmail.com>
2021-11-25 10:26:00 +0100

    Adopted to upcoming changes with Jekyll v4.3

--- lib/jekyll-remote-theme.rb
+++ lib/jekyll-remote-theme.rb
@@ -13,22 +13,18 @@ module Jekyll
   module RemoteTheme
     class DownloadError < StandardError; end
 
-    autoload :Downloader,       "jekyll-remote-theme/downloader"
-    autoload :MockGemspec,      "jekyll-remote-theme/mock_gemspec"
-    autoload :Munger,           "jekyll-remote-theme/munger"
-    autoload :Theme,            "jekyll-remote-theme/theme"
-    autoload :Reader,           "jekyll-remote-theme/reader"
-    autoload :ThemeDataReader,  "jekyll-remote-theme/theme_data_reader"
-    autoload :VERSION,          "jekyll-remote-theme/version"
+    autoload :Downloader,  "jekyll-remote-theme/downloader"
+    autoload :MockGemspec, "jekyll-remote-theme/mock_gemspec"
+    autoload :Munger,      "jekyll-remote-theme/munger"
+    autoload :Theme,       "jekyll-remote-theme/theme"
+    autoload :VERSION,     "jekyll-remote-theme/version"
 
     CONFIG_KEY  = "remote_theme"
     LOG_KEY     = "Remote Theme:"
     TEMP_PREFIX = "jekyll-remote-theme-"
 
     def self.init(site)
-      @theme = Munger.new(site).munge!
-      site.reader = Reader.new(site)
-      @theme
+      Munger.new(site).munge!
     end
   end
 end
